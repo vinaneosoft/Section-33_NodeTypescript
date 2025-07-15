58e431dc47d752fcee931cbab3c5cc24c3740066
vina <vinaneosoft@gmail.com>
2025-07-15 13:19:35 +0530

    commit

--- Deno-ts-todoAPI/controllers/todocontroller.ts
+++ Deno-ts-todoAPI/controllers/todocontroller.ts
@@ -1,23 +1,19 @@
-
+import type { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import todoimpl from "../modelimpl/todoimpl.ts"
-
 import {Todo} from '../models/todo.ts';
 
-export const createTodoMiddleware=async (ctx)=>{ 
+export const createTodoMiddleware=async (ctx:Context)=>{ 
     const body = ctx.request.body(); 
     const data = await body.value; // await the parsed value
-    console.log("Received data:", data);
+  //  console.log("Received data:", data);
     let todo= {...data, id:new Date().toISOString()} as Todo;
     const createdtodo=todoimpl.createTodo(todo);
     ctx.response.body=createdtodo
 }
-
-
-export const getAllTodosMiddleware=async (ctx)=>{
+export const getAllTodosMiddleware=async (ctx:Context)=>{
     const todos=todoimpl.getAllTodos();
     ctx.response.body=todos
 }
-
 /* 
 export const updateTodoMiddleware=(req:Request,res:Response,next:NextFunction)=>{
     const id=req.params.id;
