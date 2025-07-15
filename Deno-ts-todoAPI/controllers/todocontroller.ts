b035f2505f0ec7614ec36c0a65b42786b9c51b85
vina <vinaneosoft@gmail.com>
2025-07-15 13:23:57 +0530

    commit

--- Deno-ts-todoAPI/controllers/todocontroller.ts
+++ Deno-ts-todoAPI/controllers/todocontroller.ts
@@ -1,27 +1,29 @@
 import type { Context } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 import todoimpl from "../modelimpl/todoimpl.ts"
-import {Todo} from '../models/todo.ts';
+import { Todo } from '../models/todo.ts';
 
-export const createTodoMiddleware=async (ctx:Context)=>{ 
-    const body = ctx.request.body(); 
+export const createTodoMiddleware = async (ctx: Context) => {
+    const body = ctx.request.body();
     const data = await body.value; // await the parsed value
-  //  console.log("Received data:", data);
-    let todo= {...data, id:new Date().toISOString()} as Todo;
-    const createdtodo=todoimpl.createTodo(todo);
-    ctx.response.body=createdtodo
+    //  console.log("Received data:", data);
+    let todo = { ...data, id: new Date().toISOString() } as Todo;
+    const createdtodo = todoimpl.createTodo(todo);
+    ctx.response.body = createdtodo
 }
-export const getAllTodosMiddleware=async (ctx:Context)=>{
-    const todos=todoimpl.getAllTodos();
-    ctx.response.body=todos
+export const getAllTodosMiddleware = async (ctx: Context) => {
+    const todos = todoimpl.getAllTodos();
+    ctx.response.body = todos
+}
+
+export const updateTodoMiddleware = async (ctx: Context) => {
+    const id = ctx.params.id;
+    const body = ctx.request.body();
+    const data = await body.value;
+    const todo = data as Todo;
+    const updatedtodo = todoimpl.updateTodo(id, todo);
+    ctx.response.body = updatedtodo
 }
 /* 
-export const updateTodoMiddleware=(req:Request,res:Response,next:NextFunction)=>{
-    const id=req.params.id;
-    const todo=req.body as Todo;
-    const updatedtodo=todoimpl.updateTodo(id,todo);
-    res.send(updatedtodo)
-}
-
 export const getTodoByIdMiddleware=(req:Request,res:Response,next:NextFunction)=>{
     const foundtodo=todoimpl.getTodoById(req.params.id);
     res.send(foundtodo);
@@ -33,11 +35,11 @@ export const deleteTodoMiddleware=(req:Request,res:Response,next:NextFunction)=>
 }
  */
 
-export default 
-{
-    createTodoMiddleware,
-    getAllTodosMiddleware
-     /*updateTodoMiddleware,
-    getTodoByIdMiddleware,
-    deleteTodoMiddleware */
-}
+export default
+    {
+        createTodoMiddleware,
+        getAllTodosMiddleware
+        /*updateTodoMiddleware,
+       getTodoByIdMiddleware,
+       deleteTodoMiddleware */
+    }
